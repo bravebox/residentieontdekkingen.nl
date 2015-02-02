5e29107409bf232f23eb28782a1cccd958550ea1
Mick Schouten <bravebox@gmail.com>
2015-02-02 15:16:49 +0100

    init

---
+++ _site/config.rb
@@ -0,0 +1,8 @@
+http_path               = "/"
+sass_dir                = "_sass"
+css_dir                 = "assets/css"
+images_dir              = "assets/images"
+javascripts_dir         = "assets/js"
+
+output_style            = :compressed
+line_comments           = false
